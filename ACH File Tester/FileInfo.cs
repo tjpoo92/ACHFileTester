ed34335337a49a8e1e032f0e7e95d0c058cf4f6c
Tyler Pooschke <tjpoo92@gmail.com>
2020-06-08 09:12:25 -0500

    add entry detail addenda record

--- ACH File Tester/FileInfo.cs
+++ ACH File Tester/FileInfo.cs
@@ -51,9 +51,7 @@ namespace ACH_File_Tester
                         Console.ReadLine();
                         break;
                     case "7":
-                        Console.WriteLine(value);
-                        Console.WriteLine("This is a Entry Detail Addenda Record");
-                        //EntryDetailAddendaRecord(value);
+                        EntryDetailAddendaRecord(value);
                         Console.ReadLine();
                         break;
                     case "8":
@@ -95,9 +93,7 @@ namespace ACH_File_Tester
                         Console.WriteLine();
                         break;
                     case "7":
-                        Console.WriteLine(value);
-                        Console.WriteLine("This is a Entry Detail Addenda Record");
-                        //EntryDetailAddendaRecord(value);
+                        EntryDetailAddendaRecord(value);
                         Console.WriteLine();
                         break;
                     case "8":
@@ -182,9 +178,26 @@ namespace ACH_File_Tester
             else Console.WriteLine("This is not an Entry Detail Record");
         }
 
-        private static void EntryDetailAddendaRecord(string value)
+        private static void EntryDetailAddendaRecord(string line)
         {
-            throw new NotImplementedException();
+            if (line.Substring(0, 1) == "7")
+            {
+                Console.WriteLine(line);
+                Console.WriteLine("This is an Entry Detail Addenda Record");
+                Dictionary<string, string> entryDetailAddendaRecord = new Dictionary<string, string>
+                {
+                    { "Addenda Type Code", line.Substring(1, 2).Trim() },
+                    { "Payment Related Information", line.Substring(3, 80).Trim() },
+                    { "Addenda Sequence Number", line.Substring(83, 4).Trim() },
+                    { "Entry Detail Sequence Number", line.Substring(29, 10).Trim() },
+
+                };
+                foreach (KeyValuePair<string, string> kvp in entryDetailAddendaRecord)
+                {
+                    Console.WriteLine("{0,-30} {1,15}", kvp.Key, kvp.Value);
+                };
+            }
+            else Console.WriteLine("This is not an Entry Detail Addenda Record");
         }
 
         private static void BatchControlRecord(string line)
@@ -212,10 +225,10 @@ namespace ACH_File_Tester
             }
             else Console.WriteLine("This is not an Batch Control Record");
         }
-        
+
         private static void FileControlRecord(string line)
         {
-            if (line.Substring(0, 1) == "9" && line.Substring(1,1) != "9")
+            if (line.Substring(0, 1) == "9" && line.Substring(1, 1) != "9")
             {
                 Console.WriteLine(line);
                 Console.WriteLine("This is the File Control Record");
